7a393c97230bdc032c7e9bd95a3e6a269dfb9da5
Serhii Vietrov <vetrovsergey95@gmail.com>
2019-03-24 16:56:55 +0200

    adding files

---
+++ AviaSched/Scripts/main.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $("[data-value]").click(function () {
+        $("body,html").animate({
+            scrollTop: $("#" + $(this).data('value')).offset().top - 40
+        }, 1000);
+    });
+});
+var lang = 'en';
+function LanguageChange(lang) {
+    document.location = lang + ".index.html";
+    localStorage.setItem('SiteLanguage', lang);
+}   
+
+function nextVisit() {
+    if (localStore.getItem('SiteLanguage')) {
+        var savedLang = localStorage.getItem('SiteLanguage');
+        document.location = savedLang + ".index.html";
+    } else {
+        document.location = "en.index.html";
+    }
+}
